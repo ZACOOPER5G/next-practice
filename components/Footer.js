326b04771b1fd3bd53dc430f40729ff78c04c637
Zac Cooper <zacooper5g@outlook.com>
2022-12-01 10:00:57 +1100

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div>
+        <p>Copyright 2022 Next List</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div className="logo" >
+            <h1>Next List</h1>
+        </div>
+        <a>Home</a>
+        <a>About</a>
+        <a>Listing</a>
+    </nav>
+  )
+}
+
+export default Navbar
