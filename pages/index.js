e987c0b9ba1eb9beda292f14022f351424e730f5
Zac Cooper <zacooper5g@outlook.com>
2022-12-01 15:41:13 +1100

    began adding home styles

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Link from "next/link";
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <Link href="/listings" >See Next Listings</Link>
+    <div >
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text} >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+      <p className={styles.text} >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+      <Link href="/listings" className={styles.btn}>See Next Listings</Link>
     </div>
   )
 };
