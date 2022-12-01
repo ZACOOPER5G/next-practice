90d92b7488fcd16739157068c999737bcf292901
Zac Cooper <zacooper5g@outlook.com>
2022-12-01 16:03:23 +1100

    added automatic redirect

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,35 @@
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
-  return (
-    <div className="not-found">
-        <h1>Ooops...</h1>
-        <h2>That page cannot be found.</h2>
-        <p>Go back to the <Link href="/">homepage</Link></p>
-    </div>
-  )
+    const router = useRouter();
+    const [timer, setTimer] = useState(5);
+
+    useEffect(() => {
+        const time = setTimeout(() => {
+            setTimer(timer - 1);
+        }, 1000);
+
+        return () => {
+            clearTimeout(time);
+        }
+    }, [timer]);
+
+    useEffect(() => {
+      setTimeout(() => {
+        router.push('/');
+      }, 5000);
+    }, []);
+
+    return (
+        <div className="not-found">
+            <h1>Ooops...</h1>
+            <h2>That page cannot be found.</h2>
+            <p>Go back to the <Link className="link" href="/">homepage</Link></p>
+            <p>You will be redirected in {timer}</p>
+        </div>
+    )
 }
 
 export default NotFound
