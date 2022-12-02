224cdea6e6a440a276f4d10c7e2a4c45cdd7671b
Zac Cooper <zacooper5g@outlook.com>
2022-12-02 12:24:56 +1100

    added additional metadata

---
+++ pages/listings/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const Listings = () => {
+    return (
+        <>
+            <Head>
+                <title>Next List | Listings</title>
+            </Head>
+            <div>Listings</div>
+        </>
+    )
+};
+
+export default Listings
