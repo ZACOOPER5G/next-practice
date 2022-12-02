7649dc13f882118d9d7c8d1034539a0814670216
Zac Cooper <zacooper5g@outlook.com>
2022-12-02 14:43:16 +1100

    added listings

--- pages/listings/index.js
+++ pages/listings/index.js
@@ -1,12 +1,28 @@
 import Head from "next/head";
+import styles from "../../styles/Listings.module.css"
 
-const Listings = () => {
+export const getStaticProps = async () => {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users");
+    let data = await response.json();
+
+    return {
+        props: {
+            listings: data
+        }
+    };
+};
+
+const Listings = ({ listings }) => {
     return (
         <>
             <Head>
                 <title>Next List | Listings</title>
             </Head>
-            <div>Listings</div>
+            <div>{listings.map(item => (
+                <div key={item.id} >
+                    <h2 className={styles.single}>{ item.name }</h2>
+                </div>
+            ))}</div>
         </>
     )
 };
